5ff244c5cb61355ecc4c7063bf9281bfc3acfd17
alpha231 <juanjgarcialeon@gmail.com>
2019-07-07 04:29:52 -0500

    Update 7SEGMENTOS2.ino

--- 7SEGMENTOS2/7SEGMENTOS2.ino
+++ 7SEGMENTOS2/7SEGMENTOS2.ino
@@ -1,126 +1,126 @@
 void setup() {
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
 }
 
 void loop() {
   for(int numero=0; numero<=9; numero++){
     if(numero==0){
+        digitalWrite(0, LOW);
+        digitalWrite(1, LOW);
         digitalWrite(2, LOW);
         digitalWrite(3, LOW);
+        
         digitalWrite(4, LOW);
         digitalWrite(5, LOW);
-        
         digitalWrite(6, LOW);
         digitalWrite(7, LOW);
-        digitalWrite(8, LOW);
-        digitalWrite(9, LOW);
     }
     if(numero==1){
-        digitalWrite(2, HIGH);
+        digitalWrite(0, HIGH);
+        digitalWrite(1, LOW);
+        digitalWrite(2, LOW);
         digitalWrite(3, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(5, LOW);
         
-        digitalWrite(6, HIGH);
+        digitalWrite(4, HIGH);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
         digitalWrite(7, LOW);
-        digitalWrite(8, LOW);
-        digitalWrite(9, LOW);
     }
     if(numero==2){
+        digitalWrite(0, LOW);
+        digitalWrite(1, HIGH);
         digitalWrite(2, LOW);
-        digitalWrite(3, HIGH);
-        digitalWrite(4, LOW);
-        digitalWrite(5, LOW);
+        digitalWrite(3, LOW);
         
+        digitalWrite(4, LOW);
+        digitalWrite(5, HIGH);
         digitalWrite(6, LOW);
-        digitalWrite(7, HIGH);
-        digitalWrite(8, LOW);
-        digitalWrite(9, LOW);
+        digitalWrite(7, LOW);
     }
     if(numero==3){
-        digitalWrite(2, HIGH);
-        digitalWrite(3, HIGH);
-        digitalWrite(4, LOW);
-        digitalWrite(5, LOW);
+        digitalWrite(0, HIGH);
+        digitalWrite(1, HIGH);
+        digitalWrite(2, LOW);
+        digitalWrite(3, LOW);
         
-        digitalWrite(6, HIGH);
-        digitalWrite(7, HIGH);
-        digitalWrite(8, LOW);
-        digitalWrite(9, LOW);
+        digitalWrite(4, HIGH);
+        digitalWrite(5, HIGH);
+        digitalWrite(6, LOW);
+        digitalWrite(7, LOW);
     }
     if(numero==4){
-        digitalWrite(2, LOW);
+        digitalWrite(0, LOW);
+        digitalWrite(1, LOW);
+        digitalWrite(2, HIGH);
         digitalWrite(3, LOW);
-        digitalWrite(4, HIGH);
-        digitalWrite(5, LOW);
         
-        digitalWrite(6, LOW);
+        digitalWrite(4, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(6, HIGH);
         digitalWrite(7, LOW);
-        digitalWrite(8, HIGH);
-        digitalWrite(9, LOW);
     }
     if(numero==5){
+        digitalWrite(0, HIGH);
+        digitalWrite(1, LOW);
         digitalWrite(2, HIGH);
         digitalWrite(3, LOW);
+        
         digitalWrite(4, HIGH);
         digitalWrite(5, LOW);
-        
         digitalWrite(6, HIGH);
         digitalWrite(7, LOW);
-        digitalWrite(8, HIGH);
-        digitalWrite(9, LOW);
     }
     if(numero==6){
-        digitalWrite(2, LOW);
-        digitalWrite(3, HIGH);
-        digitalWrite(4, HIGH);
-        digitalWrite(5, LOW);
+        digitalWrite(0, LOW);
+        digitalWrite(1, HIGH);
+        digitalWrite(2, HIGH);
+        digitalWrite(3, LOW);
         
-        digitalWrite(6, LOW);
-        digitalWrite(7, HIGH);
-        digitalWrite(8, HIGH);
-        digitalWrite(9, LOW);
+        digitalWrite(4, LOW);
+        digitalWrite(5, HIGH);
+        digitalWrite(6, HIGH);
+        digitalWrite(7, LOW);
     }
     if(numero==7){
+        digitalWrite(0, HIGH);
+        digitalWrite(1, HIGH);
         digitalWrite(2, HIGH);
-        digitalWrite(3, HIGH);
-        digitalWrite(4, HIGH);
-        digitalWrite(5, LOW);
+        digitalWrite(3, LOW);
         
+        digitalWrite(4, HIGH);
+        digitalWrite(5, HIGH);
         digitalWrite(6, HIGH);
-        digitalWrite(7, HIGH);
-        digitalWrite(8, HIGH);
-        digitalWrite(9, LOW);
+        digitalWrite(7, LOW);
     }
     if(numero==8){
+        digitalWrite(0, LOW);
+        digitalWrite(1, LOW);
         digitalWrite(2, LOW);
-        digitalWrite(3, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(5, HIGH);
+        digitalWrite(3, HIGH);
         
+        digitalWrite(4, LOW);
+        digitalWrite(5, LOW);
         digitalWrite(6, LOW);
-        digitalWrite(7, LOW);
-        digitalWrite(8, LOW);
-        digitalWrite(9, HIGH);
+        digitalWrite(7, HIGH);
     }
     if(numero==9){
-        digitalWrite(2, HIGH);
-        digitalWrite(3, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(5, HIGH);
+        digitalWrite(0, HIGH);
+        digitalWrite(1, LOW);
+        digitalWrite(2, LOW);
+        digitalWrite(3, HIGH);
         
-        digitalWrite(6, HIGH);
-        digitalWrite(7, LOW);
-        digitalWrite(8, LOW);
-        digitalWrite(9, HIGH);
+        digitalWrite(4, HIGH);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(7, HIGH);
     }
-    delay(500);
+    delay(300);
   }
 }
