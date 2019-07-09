ded33be2fcd9ef9a50d8e7f0916c66a4f452e726
alpha231 <juanjgarcialeon@gmail.com>
2019-07-09 01:12:12 -0500

    pinFormat DHT-7S-FTR_event

--- DHT-7S-FTR_event/DHT-7S-FTR_event.ino
+++ DHT-7S-FTR_event/DHT-7S-FTR_event.ino
@@ -5,20 +5,30 @@
 #define PINdht A1
 #define PINbutton A2
 #define PINled A3
+
+#define A_1 0
+#define B_1 1
+#define C_1 2
+#define D_1 3
+#define A_2 4
+#define B_2 5
+#define C_2 6
+#define D_2 7
+
 #define DHTTYPE DHT11
 
 DHT dht(PINdht, DHTTYPE);
 EvtManager mgr;
 
 void setup() {
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
+  pinMode(A_1, OUTPUT);
+  pinMode(B_1, OUTPUT);
+  pinMode(C_1, OUTPUT);
+  pinMode(D_1, OUTPUT);
+  pinMode(A_2, OUTPUT);
+  pinMode(B_2, OUTPUT);
+  pinMode(C_2, OUTPUT);
+  pinMode(D_2, OUTPUT);
   pinMode(PINlight, INPUT);
   pinMode(PINbutton, INPUT);
   pinMode(PINled, OUTPUT);
@@ -56,7 +66,7 @@ void ReadHumidity() {
 void ReadLight() {
   int sensorValue = analogRead(PINlight);
   float l = 0;
-  l = (100-(sensorValue * 100.0) / 1023.0);
+  l = (100 - (sensorValue * 100.0) / 1023.0);
   Serial.println(l);
   Display(l);
 }
@@ -66,7 +76,7 @@ void ReadTemp() {
   Serial.println(n);
   Display(n);
 }
-void BlinkLed(){
+void BlinkLed() {
   //delay(1000);
   digitalWrite(PINled, HIGH);
   delay(100);
@@ -75,142 +85,142 @@ void BlinkLed(){
 void Display(int n) {
   if ((n >= 0 && n <= 9) || n % 10 == 0) {
     if (n % 10 == 0) {
-      digitalWrite(0, LOW);
-      digitalWrite(1, LOW);
-      digitalWrite(2, LOW);
-      digitalWrite(3, LOW);
+      digitalWrite(A_1, LOW);
+      digitalWrite(B_1, LOW);
+      digitalWrite(C_1, LOW);
+      digitalWrite(D_1, LOW);
     }
     if (n >= 0 && n <= 9) {
-      digitalWrite(4, LOW);
-      digitalWrite(5, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
+      digitalWrite(A_2, LOW);
+      digitalWrite(B_2, LOW);
+      digitalWrite(C_2, LOW);
+      digitalWrite(D_2, LOW);
     }
   }
   if ((n >= 10 && n <= 19) || n % 10 == 1) {
     if (n % 10 == 1) {
-      digitalWrite(0, HIGH);
-      digitalWrite(1, LOW);
-      digitalWrite(2, LOW);
-      digitalWrite(3, LOW);
+      digitalWrite(A_1, HIGH);
+      digitalWrite(B_1, LOW);
+      digitalWrite(C_1, LOW);
+      digitalWrite(D_1, LOW);
     }
     if (n >= 10 && n <= 19) {
-      digitalWrite(4, HIGH);
-      digitalWrite(5, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
+      digitalWrite(A_2, HIGH);
+      digitalWrite(B_2, LOW);
+      digitalWrite(C_2, LOW);
+      digitalWrite(D_2, LOW);
     }
   }
   if ((n >= 20 && n <= 29) || n % 10 == 2) {
     if (n % 10 == 2) {
-      digitalWrite(0, LOW);
-      digitalWrite(1, HIGH);
-      digitalWrite(2, LOW);
-      digitalWrite(3, LOW);
+      digitalWrite(A_1, LOW);
+      digitalWrite(B_1, HIGH);
+      digitalWrite(C_1, LOW);
+      digitalWrite(D_1, LOW);
     }
     if (n >= 20 && n <= 29) {
-      digitalWrite(4, LOW);
-      digitalWrite(5, HIGH);
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
+      digitalWrite(A_2, LOW);
+      digitalWrite(B_2, HIGH);
+      digitalWrite(C_2, LOW);
+      digitalWrite(D_2, LOW);
     }
   }
   if ((n >= 30 && n <= 39) || n % 10 == 3) {
     if (n % 10 == 3) {
-      digitalWrite(0, HIGH);
-      digitalWrite(1, HIGH);
-      digitalWrite(2, LOW);
-      digitalWrite(3, LOW);
+      digitalWrite(A_1, HIGH);
+      digitalWrite(B_1, HIGH);
+      digitalWrite(C_1, LOW);
+      digitalWrite(D_1, LOW);
     }
     if (n >= 30 && n <= 39) {
-      digitalWrite(4, HIGH);
-      digitalWrite(5, HIGH);
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
+      digitalWrite(A_2, HIGH);
+      digitalWrite(B_2, HIGH);
+      digitalWrite(C_2, LOW);
+      digitalWrite(D_2, LOW);
     }
   }
   if ((n >= 40 && n <= 49) || n % 10 == 4) {
     if (n % 10 == 4) {
-      digitalWrite(0, LOW);
-      digitalWrite(1, LOW);
-      digitalWrite(2, HIGH);
-      digitalWrite(3, LOW);
+      digitalWrite(A_1, LOW);
+      digitalWrite(B_1, LOW);
+      digitalWrite(C_1, HIGH);
+      digitalWrite(D_1, LOW);
     }
     if (n >= 40 && n <= 49) {
-      digitalWrite(4, LOW);
-      digitalWrite(5, LOW);
-      digitalWrite(6, HIGH);
-      digitalWrite(7, LOW);
+      digitalWrite(A_2, LOW);
+      digitalWrite(B_2, LOW);
+      digitalWrite(C_2, HIGH);
+      digitalWrite(D_2, LOW);
     }
   }
   if ((n >= 50 && n <= 59) || n % 10 == 5) {
     if (n % 10 == 5) {
-      digitalWrite(0, HIGH);
-      digitalWrite(1, LOW);
-      digitalWrite(2, HIGH);
-      digitalWrite(3, LOW);
+      digitalWrite(A_1, HIGH);
+      digitalWrite(B_1, LOW);
+      digitalWrite(C_1, HIGH);
+      digitalWrite(D_1, LOW);
     }
     if (n >= 50 && n <= 59) {
-      digitalWrite(4, HIGH);
-      digitalWrite(5, LOW);
-      digitalWrite(6, HIGH);
-      digitalWrite(7, LOW);
+      digitalWrite(A_2, HIGH);
+      digitalWrite(B_2, LOW);
+      digitalWrite(C_2, HIGH);
+      digitalWrite(D_2, LOW);
     }
   }
   if ((n >= 60 && n <= 69) || n % 10 == 6) {
     if (n % 10 == 6) {
-      digitalWrite(0, LOW);
-      digitalWrite(1, HIGH);
-      digitalWrite(2, HIGH);
-      digitalWrite(3, LOW);
+      digitalWrite(A_1, LOW);
+      digitalWrite(B_1, HIGH);
+      digitalWrite(C_1, HIGH);
+      digitalWrite(D_1, LOW);
     }
     if (n >= 60 && n <= 69) {
-      digitalWrite(4, LOW);
-      digitalWrite(5, HIGH);
-      digitalWrite(6, HIGH);
-      digitalWrite(7, LOW);
+      digitalWrite(A_2, LOW);
+      digitalWrite(B_2, HIGH);
+      digitalWrite(C_2, HIGH);
+      digitalWrite(D_2, LOW);
     }
   }
   if ((n >= 70 && n <= 79) || n % 10 == 7) {
     if (n % 10 == 7) {
-      digitalWrite(0, HIGH);
-      digitalWrite(1, HIGH);
-      digitalWrite(2, HIGH);
-      digitalWrite(3, LOW);
+      digitalWrite(A_1, HIGH);
+      digitalWrite(B_1, HIGH);
+      digitalWrite(C_1, HIGH);
+      digitalWrite(D_1, LOW);
     }
     if (n >= 70 && n <= 79) {
-      digitalWrite(4, HIGH);
-      digitalWrite(5, HIGH);
-      digitalWrite(6, HIGH);
-      digitalWrite(7, LOW);
+      digitalWrite(A_2, HIGH);
+      digitalWrite(B_2, HIGH);
+      digitalWrite(C_2, HIGH);
+      digitalWrite(D_2, LOW);
     }
   }
   if ((n >= 80 && n <= 89) || n % 10 == 8) {
     if (n % 10 == 8) {
-      digitalWrite(0, LOW);
-      digitalWrite(1, LOW);
-      digitalWrite(2, LOW);
-      digitalWrite(3, HIGH);
+      digitalWrite(A_1, LOW);
+      digitalWrite(B_1, LOW);
+      digitalWrite(C_1, LOW);
+      digitalWrite(D_1, HIGH);
     }
     if (n >= 80 && n <= 89) {
-      digitalWrite(4, LOW);
-      digitalWrite(5, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(7, HIGH);
+      digitalWrite(A_2, LOW);
+      digitalWrite(B_2, LOW);
+      digitalWrite(C_2, LOW);
+      digitalWrite(D_2, HIGH);
     }
   }
   if ((n >= 90 && n <= 99) || n % 10 == 9) {
     if (n % 10 == 9) {
-      digitalWrite(0, HIGH);
-      digitalWrite(1, LOW);
-      digitalWrite(2, LOW);
-      digitalWrite(3, HIGH);
+      digitalWrite(A_1, HIGH);
+      digitalWrite(B_1, LOW);
+      digitalWrite(C_1, LOW);
+      digitalWrite(D_1, HIGH);
     }
     if (n >= 90 && n <= 99) {
-      digitalWrite(4, HIGH);
-      digitalWrite(5, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(7, HIGH);
+      digitalWrite(A_2, HIGH);
+      digitalWrite(B_2, LOW);
+      digitalWrite(C_2, LOW);
+      digitalWrite(D_2, HIGH);
     }
   }
 }
